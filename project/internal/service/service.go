624223a2c1f531bb5e9e947ddab8a66f816eacb3
Evgeniya Salnikova <ypsalniko1@mts.ru>
2025-04-18 15:54:53 +0300

    hw16: Примитивы синхронизации

--- project/internal/service/service.go
+++ project/internal/service/service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"sync"
+
 	"project/internal/models"
 	"project/internal/repository"
 )
@@ -8,6 +10,26 @@ import (
 func CreateModel() {
 	task := models.NewTask("new task", "health")
 	tag := models.NewTag("health")
-	repository.FillSlice(task)
-	repository.FillSlice(tag)
+
+	taskPointer := &task
+	tagPointer := &tag
+
+	taskChannel := make(chan repository.Item, 10)
+	tagChannel := make(chan repository.Item, 10)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go repository.FillSlice(taskChannel, &wg)
+
+	wg.Add(1)
+	go repository.FillSlice(tagChannel, &wg)
+
+	taskChannel <- taskPointer
+	tagChannel <- tagPointer
+
+	close(taskChannel)
+	close(tagChannel)
+
+	wg.Wait()
 }
--- project/main/main.go
+++ project/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"project/internal/repository"
 	"project/internal/service"
@@ -9,10 +10,16 @@ import (
 
 func main() {
 
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go repository.LogChanges(wg)
+
 	for i := 0; i < 10; i++ {
 		service.CreateModel()
 	}
 
 	fmt.Println(repository.TaskSlice)
 	fmt.Println(repository.TagSlice)
+
+	wg.Wait()
 }
